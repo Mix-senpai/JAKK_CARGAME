7b4d1f46a4d65241475a94352e983b651d922611
MIX <lemonmixsenpai@gmail.com>
2025-11-28 21:45:45 +0800

    Signed-off-by: MIX <lemonmixsenpai@gmail.com>

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Checkpoint : MonoBehaviour 
+{
+	public int checkpointIndex;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+		{
+			RaceManager.Instance.CheckpointReached(checkpointIndex);
+		}
+	}
+}
